539629c7a2af7864c7ae45309869479697fe6617
KhanhNM-gif <81376454+KhanhNM-gif@users.noreply.github.com>
2021-12-12 23:33:41 +0700

    mahatan

--- Bai2KPDL/Program.cs
+++ Bai2KPDL/Program.cs
@@ -119,6 +119,7 @@ namespace Bai2KPDL
                     foreach (var c in tam)
                     {
                         float kc = (float)Math.Sqrt((c.x - item.Value.x) * (c.x - item.Value.x) + (c.y - item.Value.y) * (c.y - item.Value.y));
+                        //float kc = (float)Math.Abs((c.x - item.Value.x) + (c.y - item.Value.y) );
                         Console.Write(Math.Round(kc, 2).ToString().PadRight(10));
 
                         if (min > kc)
